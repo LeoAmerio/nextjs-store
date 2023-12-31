d47b2b50825bbbd150283834bdefae9455ffc4df
Leonardo Amerio <leoamerio14@gmail.com>
2023-12-31 11:33:07 -0300

    Aplicando Sass

---
+++ src/app/components/home/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.sass';
+
+export const Hero = () => {
+  return (
+    <section>
+      <h1>Hero</h1>
+    </section>
+  );
+};
---
+++ src/app/components/home/MainProducts/index.ts
@@ -0,0 +1,3 @@
+
+
+export {MainProducts} from './MainProducts';
